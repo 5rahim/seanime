7f009fab484ba4403336fa995ef81d2337919c3b
5rahim <bonfohzaki@gmail.com>
2023-11-30 16:15:08 -0500

    delete web build

--- web/_next/static/chunks/app/(main)/qbittorrent/page-177f7611544cac13.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[77],{32329:function(r,e,t){Promise.resolve().then(t.bind(t,66282))},66282:function(r,e,t){"use strict";t.r(e),t.d(e,{default:function(){return Page}});var n=t(57437),o=t(23890),i=t(751);function Page(){var r,e;let t=(0,o.Dv)(i.X),u=null==t?void 0:t.settings;return u?(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("div",{className:"w-[80%] h-[calc(100vh-15rem)] rounded-xl border border-[--border] overflow-hidden mx-auto mt-10 ring-1 ring-[--border] ring-offset-2",children:(0,n.jsx)("iframe",{src:"http://".concat(null===(r=u.torrent)||void 0===r?void 0:r.qbittorrentHost,":").concat(String(null===(e=u.torrent)||void 0===e?void 0:e.qbittorrentPort)),className:"w-full h-full"})})}):null}},751:function(r,e,t){"use strict";t.d(e,{X:function(){return o}});var n=t(57685);let o=(0,n.sn)(void 0)},30622:function(r,e,t){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=t(2265),o=Symbol.for("react.element"),i=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,s=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,l={key:!0,ref:!0,__self:!0,__source:!0};function q(r,e,t){var n,i={},c=null,f=null;for(n in void 0!==t&&(c=""+t),void 0!==e.key&&(c=""+e.key),void 0!==e.ref&&(f=e.ref),e)u.call(e,n)&&!l.hasOwnProperty(n)&&(i[n]=e[n]);if(r&&r.defaultProps)for(n in e=r.defaultProps)void 0===i[n]&&(i[n]=e[n]);return{$$typeof:o,type:r,key:c,ref:f,props:i,_owner:s.current}}e.Fragment=i,e.jsx=q,e.jsxs=q},57437:function(r,e,t){"use strict";r.exports=t(30622)}},function(r){r.O(0,[890,685,971,472,744],function(){return r(r.s=32329)}),_N_E=r.O()}]);
