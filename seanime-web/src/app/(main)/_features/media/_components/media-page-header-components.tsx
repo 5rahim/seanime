174c7eccbc3ee6f869c2554cfbfb5fe4df2f1d02
5rahim <bonfohzaki@gmail.com>
2024-05-19 21:36:33 -0400

    fix score

--- seanime-web/src/app/(main)/_features/media/_components/media-page-header-components.tsx
+++ seanime-web/src/app/(main)/_features/media/_components/media-page-header-components.tsx
@@ -324,16 +324,16 @@ export function MediaPageHeaderScoreAndProgress({ score, progress, episodes }: {
 }) {
 
     const scoreColor = score ? (
-        score < 5 ? "bg-red-500" :
-            score < 7 ? "bg-gray-500" :
-                score < 9 ? "bg-green-500" :
+        score < 50 ? "bg-red-500" :
+            score < 70 ? "bg-gray-500" :
+                score < 85 ? "bg-green-500" :
                     "bg-brand-500 text-white"
     ) : ""
 
     return (
         <>
             {!!score && <Badge leftIcon={<BiStar />} size="xl" intent="primary-solid" className={scoreColor}>
-                {score}
+                {score / 10}
             </Badge>}
             <Badge
                 size="xl"
