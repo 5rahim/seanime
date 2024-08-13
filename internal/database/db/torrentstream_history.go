7851bd2c811c906b8494526a8606b862fb4f25d9
5rahim <bonfohzaki@gmail.com>
2024-08-13 12:00:43 -0400

    fix sql

--- internal/database/db/torrentstream_history.go
+++ internal/database/db/torrentstream_history.go
@@ -9,14 +9,14 @@ func (db *Database) TrimTorrentstreamHistory() {
 		var count int64
 		err := db.gormdb.Model(&models.TorrentstreamHistory{}).Count(&count).Error
 		if err != nil {
-			db.Logger.Error().Err(err).Msg("database: Failed to count local file entries")
+			db.Logger.Error().Err(err).Msg("database: Failed to count torrent stream history entries")
 			return
 		}
 		if count > 10 {
 			// Leave 5 entries
-			err = db.gormdb.Delete(&models.TorrentstreamHistory{}, "id IN (SELECT id FROM local_files ORDER BY id ASC LIMIT ?)", count-5).Error
+			err = db.gormdb.Delete(&models.TorrentstreamHistory{}, "id IN (SELECT id FROM torrentstream_histories ORDER BY id ASC LIMIT ?)", count-5).Error
 			if err != nil {
-				db.Logger.Error().Err(err).Msg("database: Failed to delete old local file entries")
+				db.Logger.Error().Err(err).Msg("database: Failed to delete old torrent stream history entries")
 				return
 			}
 		}
