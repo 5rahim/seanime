deeff0616b54ded28466b27a2cc7f14f30eea4b4
5rahim <bonfohzaki@gmail.com>
2024-08-22 15:06:59 -0400

    feat: gradient correction

--- seanime-web/src/app/(main)/_features/custom-ui/item-bottom-gradients.tsx
+++ seanime-web/src/app/(main)/_features/custom-ui/item-bottom-gradients.tsx
@@ -13,14 +13,14 @@ export function MediaCardBodyBottomGradient() {
     if (!!ts.libraryScreenCustomBackgroundImage || ts.hasCustomBackgroundColor) {
         return (
             <div
-                className="z-[5] absolute bottom-0 w-full h-[40%] opacity-80 bg-gradient-to-t from-[--background] to-transparent"
+                className="z-[5] absolute bottom-0 w-full h-[40%] opacity-80 bg-gradient-to-t from-[#0c0c0c] to-transparent"
             />
         )
     }
 
     return (
         <div
-            className="z-[5] absolute bottom-0 w-full opacity-90 to-40% h-[50%] bg-gradient-to-t from-[--background] to-transparent"
+            className="z-[5] absolute bottom-0 w-full opacity-90 to-40% h-[50%] bg-gradient-to-t from-[#0c0c0c] to-transparent"
         />
     )
 }
@@ -30,17 +30,21 @@ export function EpisodeItemBottomGradient() {
 
     const ts = useThemeSettings()
 
-    if (!!ts.libraryScreenCustomBackgroundImage || ts.hasCustomBackgroundColor) {
-        return (
-            <div
-                className="z-[1] absolute bottom-0 w-full h-full opacity-80 md:h-[60%] bg-gradient-to-t from-[--background] to-transparent"
-            />
-        )
+    // if (!!ts.libraryScreenCustomBackgroundImage || ts.hasCustomBackgroundColor) {
+    //     return (
+    //         <div
+    //             className="z-[1] absolute bottom-0 w-full h-full opacity-80 md:h-[60%] bg-gradient-to-t from-[--background] to-transparent"
+    //         />
+    //     )
+    // }
+
+    if (ts.useLegacyEpisodeCard) {
+        return <div
+            className="z-[1] absolute bottom-0 w-full h-full opacity-90 md:h-[80%] bg-gradient-to-t from-[#0c0c0c] to-transparent"
+        />
     }
 
-    return (
-        <div
-            className="z-[1] absolute bottom-0 w-full h-full opacity-90 md:h-[80%] bg-gradient-to-t from-[--background] to-transparent"
-        />
-    )
+    return <div
+        className="z-[1] absolute bottom-0 w-full h-full opacity-50 md:h-[70%] bg-gradient-to-t from-[#0c0c0c] to-transparent"
+    />
 }
