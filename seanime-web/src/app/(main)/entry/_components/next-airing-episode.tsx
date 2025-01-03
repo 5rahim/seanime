a3bb5fae13c69bd16990253be3a0b2157ecf4cbc
5rahim <bonfohzaki@gmail.com>
2025-01-03 21:03:57 +0000

    fixes

--- seanime-web/src/app/(main)/entry/_components/next-airing-episode.tsx
+++ seanime-web/src/app/(main)/entry/_components/next-airing-episode.tsx
@@ -1,9 +1,9 @@
 import { AL_BaseAnime } from "@/api/generated/types"
-import { Badge } from "@/components/ui/badge"
 import { cn } from "@/components/ui/core/styling"
 import { ThemeMediaPageInfoBoxSize, useThemeSettings } from "@/lib/theme/hooks"
 import { addSeconds, format, formatDistanceToNow } from "date-fns"
 import React from "react"
+import { FiCalendar } from "react-icons/fi"
 
 export function NextAiringEpisode(props: { media: AL_BaseAnime }) {
     const distance = formatDistanceToNow(addSeconds(new Date(), props.media.nextAiringEpisode?.timeUntilAiring || 0), { addSuffix: true })
@@ -13,16 +13,13 @@ export function NextAiringEpisode(props: { media: AL_BaseAnime }) {
         {!!props.media.nextAiringEpisode && (
             <div
                 className={cn(
-                    "flex gap-2 items-center justify-center",
+                    "flex gap-2 items-center justify-center text-lg",
                     ts.mediaPageBannerInfoBoxSize === ThemeMediaPageInfoBoxSize.Fluid && "justify-start",
                 )}
             >
-                <p className="text-xl min-[2000px]:text-xl">Episode <Badge
-                    size="lg"
-                    className="rounded-md px-1.5"
-                >{props.media.nextAiringEpisode?.episode}</Badge> {distance} <span
-                    className="text-[--muted]"
-                >({day})</span></p>
+                <span className="font-semibold">Episode {props.media.nextAiringEpisode?.episode}</span> {distance}
+                <FiCalendar className="text-lg text-[--muted]" />
+                <span className="text-[--muted]">{day}</span>
             </div>
         )}
     </>
