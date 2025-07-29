1695e5fe6f20ded1d9f46c5f5271760ad422d719
5rahim <bonfohzaki@gmail.com>
2025-07-29 11:56:44 +0000

    downgrade nextjs for real this time

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	Version              = "2.9.4-rc.0"
+	Version              = "2.9.4"
 	VersionName          = "Natsu"
 	GcTime               = time.Minute * 30
 	ConfigFileName       = "config.toml"
