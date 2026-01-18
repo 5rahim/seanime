8a02c8509caefeed8993134ad71f84882a493c28
5rahim <bonfohzaki@gmail.com>
2026-01-18 15:32:48 +0100

    update version name

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	Version              = "3.3.1"
-	VersionName          = "Arashi"
+	VersionName          = "Rensa"
 	GcTime               = time.Minute * 30
 	ConfigFileName       = "config.toml"
 	MalClientId          = "51cb4294feb400f3ddc66a30f9b9a00f"
